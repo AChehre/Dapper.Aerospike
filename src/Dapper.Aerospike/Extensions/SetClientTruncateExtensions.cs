569d61fb147942d39020b349ffc7c69a45343255
ahmad <achehre@yahoo.com>
2021-07-03 20:00:13 +0430

    should be able to truncate set

---
+++ src/Dapper.Aerospike/Extensions/SetClientTruncateExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Aerospike.Client;
+
+namespace Dapper.Aerospike
+{
+    public static class SetClientTruncateExtensions
+    {
+        public static void Truncate<TEntity>(this ISet<TEntity> set, DateTime? beforeLastUpdate = null)
+        {
+            set.Client.Truncate(new InfoPolicy(), set.Namespace, set.Name, beforeLastUpdate);
+        }
+
+        public static void Truncate<TEntity>(this ISet<TEntity> set,
+                                             InfoPolicy writePolicy,
+                                             DateTime? beforeLastUpdate = null)
+        {
+            set.Client.Truncate(writePolicy, set.Namespace, set.Name, beforeLastUpdate);
+        }
+    }
+}
